9576aa4dda83b1eaf82432d56e182db261252cf5
JesusGPriego <suleware@gmail.com>
2024-10-11 14:07:07 +0200

    around AOP impl

--- src/main/java/com/suleware/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/suleware/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.suleware.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -47,6 +49,25 @@ public class GreetingAspect {
         String args = Arrays.toString(joinPoint.getArgs());
         String info = String.format("AfterThrowing: %s; args: %s", method, args);
         logger.info(info);
+    }
+
+    @Around("execution(String com.suleware.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        String beforeInfo = String.format("Method: %s() called with params %s", method, args);
+        logger.info(beforeInfo);
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+            String afterInfo = String.format("Method: %s() returns %s", method, result);
+            logger.info(afterInfo);
+        } catch (Throwable e) {
+            String errorMessage = String.format("Error calling %s(), cause: %s", method, e.getMessage());
+            logger.error(errorMessage);
+            throw e;
+        }
+        return result;
     }
 
 }
